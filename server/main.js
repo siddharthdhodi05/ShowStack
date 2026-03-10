9d4f6bbc43ad96df0eea54227a7cf142de3c581e
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2026-03-10 15:04:12 +0530

    Step 4 completed:     - Installed dotenv     - Added .env file in the root directory with PORT and NODE_ENV.       Below is an example:
        NODE_ENV=development
        PORT=5000
    
        - Modified the main file to read values from the .env file

--- server/main.js
+++ server/main.js
@@ -1,13 +1,16 @@
 import express from "express";
-
-const port = 5005;
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
+
+const port = process.env.PORT || 5004;
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${port}`);
 });

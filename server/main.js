d6246e8d57f1e781bbe00ff8fe72d829b1392be3
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2026-03-10 16:08:02 +0530

    Step 6 completed:     - ## Added absolute path support     - Installed the babel/preset-env (npm i -D @babel/preset-env)     - Added .babelrc file     - Added "imports" property in the package.json file     - Changed imports in the main.js file

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/db.config.js";
+import connectDB from "#config/db.config.js";
 
 const app = express();
 
